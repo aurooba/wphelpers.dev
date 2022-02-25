dcf41f9350c7deb7d6c6f3150b11edf8ddb4859f
Aurooba Ahmed <hey@aurooba.com>
2022-02-25 15:54:11 -0700

    add some meta

--- pages/index.js
+++ pages/index.js
@@ -35,6 +35,43 @@ export default function Home() {
 			<Head>
 				<title>The latest WordPress version is {latestWP}</title>
 				<link rel="icon" href="/favicon.ico" />
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<meta charSet="utf-8" />
+				<meta
+					name="description"
+					content="Displays the latest WordPress version available."></meta>
+				<meta
+					property="og:title"
+					content={`The latest WordPress version is ${latestWP}`}
+					key="ogtitle"
+				/>
+				<meta
+					property="og:description"
+					content="Displays the latest WordPress version available."
+					key="ogdesc"
+				/>
+				{/* Twitter */}
+				<meta name="twitter:card" content="summary" key="twcard" />
+				<meta name="twitter:creator" content="@aurooba" key="twhandle" />
+
+				{/* Open Graph */}
+				<meta property="og:url" content="https://www.latestwp.is" key="ogurl" />
+				<meta property="og:image" content="/latestwpis.png" key="ogimage" />
+				<meta
+					property="og:site_name"
+					content={`The latest WordPress version is ${latestWP}`}
+					key="ogsitename"
+				/>
+				<meta
+					property="og:title"
+					content={`The latest WordPress version is ${latestWP}`}
+					key="ogtitle"
+				/>
+				<meta
+					property="og:description"
+					content="Displays the latest WordPress version available."
+					key="ogdesc"
+				/>
 			</Head>
 
 			<main>

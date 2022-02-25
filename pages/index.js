1cc8942431d499586d582818e7361f1998e3c15c
Aurooba Ahmed <hey@aurooba.com>
2022-02-25 14:09:20 -0700

    remove fallback from title to check

--- pages/index.js
+++ pages/index.js
@@ -33,16 +33,13 @@ export default function Home() {
 	return (
 		<div className="container">
 			<Head>
-				<title>
-					The latest WordPress version is {latestWP ? latestWP : "5.9.1"}
-				</title>
+				<title>The latest WordPress version is {latestWP}</title>
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 
 			<main>
 				<section class="main-feature">
 					<p>The latest WordPress version is:</p>
-					{/* <h1 className="version">{latestWP ? latestWP : "5.9.1"}</h1> */}
 					<h1 className="version">{latestWP}</h1>
 				</section>
 			</main>

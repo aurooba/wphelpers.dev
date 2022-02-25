38cb00e38eea51438a56c649d8c96bb61263f017
Aurooba Ahmed <hey@aurooba.com>
2022-02-25 14:08:41 -0700

    add fathom

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,27 @@
 import "@styles/globals.scss";
+import { useRouter } from "next/router";
+import * as Fathom from "fathom-client";
 
 function Application({ Component, pageProps }) {
+	const router = useRouter();
+
+	useEffect(() => {
+		Fathom.load("JALFHTEN", {
+			includedDomains: ["latestwp.is"],
+		});
+
+		function onRouteChangeComplete() {
+			Fathom.trackPageview();
+		}
+		// Record a pageview when route changes
+		router.events.on("routeChangeComplete", onRouteChangeComplete);
+
+		// Unassign event listener
+		return () => {
+			router.events.off("routeChangeComplete", onRouteChangeComplete);
+		};
+	}, []);
+
 	return <Component {...pageProps} />;
 }
 

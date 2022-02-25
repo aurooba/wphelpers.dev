7e16b957a05a6457b74fa933e729abef2c74f6fa
Aurooba Ahmed <hey@aurooba.com>
2022-02-25 13:56:52 -0700

    call wp api function and set up content

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,47 @@
+/**
+ * External Dependencies
+ */
 import Head from "next/head";
+import React, { useEffect, useState } from "react";
+
+/**
+ * Internal Dependencies
+ */
 import Footer from "@components/Footer";
 
 export default function Home() {
-	const latestWP = "";
+	const MINUTE_MS = 60000;
+	const [latestWP, setLatestWP] = useState("");
+
+	useEffect(() => {
+		const interval = setInterval(() => {
+			fetch("/.netlify/functions/wp")
+				.then(async (response) => response.json())
+				.then((response) => {
+					setLatestWP(response.version);
+				})
+				.catch((error) => {
+					console.log("error: " + error);
+				});
+		}, MINUTE_MS);
+
+		return () => clearInterval(interval);
+	}, []);
 	return (
 		<div className="container">
 			<Head>
-				<title>The latest WordPress version is {latestWP}</title>
+				<title>
+					The latest WordPress version is {latestWP ? latestWP : "5.9.1"}
+				</title>
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 
-			<main></main>
+			<main>
+				<section class="main-feature">
+					<p>The latest WordPress version is:</p>
+					<h1 className="version">{latestWP ? latestWP : "5.9.1"}</h1>
+				</section>
+			</main>
 
 			<Footer />
 		</div>

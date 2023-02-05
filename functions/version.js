e79446702d0d68d4a67654ed09f1dbaf74080cf6
Aurooba Ahmed <hey@aurooba.com>
2023-02-04 21:32:40 -0700

    add endpoint for wp version

---
+++ functions/version.js
@@ -0,0 +1,19 @@
+const fetch = require("node-fetch");
+
+const API_ENDPOINT = "http://api.wordpress.org/core/version-check/1.7/?";
+
+exports.handler = async () => {
+	const response = await fetch(API_ENDPOINT, {
+		mode: "no-cors",
+	})
+		.then((response) => response.json())
+		.then((data) => ({
+			headers: {
+				"Access-Control-Allow-Origin": "*",
+			},
+			statusCode: 200,
+			body: JSON.stringify(data["offers"][0]["version"]),
+		}))
+		.catch((error) => ({ statusCode: 422, body: String(error) }));
+	return response;
+};

71b4cce97ddd79f5ae3dfb365cb8e8c664db6738
Aurooba Ahmed <hey@aurooba.com>
2023-02-04 22:00:27 -0700

    schedule deploys for MWF

---
+++ functions/scheduled-deploy.js
@@ -0,0 +1,21 @@
+import fetch from "node-fetch";
+import { schedule } from "@netlify/functions";
+
+// This is a sample build hook URL
+const BUILD_HOOK = "https://api.netlify.com/build_hooks/abc123def456";
+
+// Schedules the handler function to run at midnight on
+// Mondays, Wednesday, and Friday
+const handler = schedule("0 0 * * 1,3,5", async () => {
+	await fetch(BUILD_HOOK, {
+		method: "POST",
+	}).then((response) => {
+		console.log("Build hook response:", response);
+	});
+
+	return {
+		statusCode: 200,
+	};
+});
+
+export { handler };

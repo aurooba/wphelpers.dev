551072778babc205546cca9fa0b67981f62474fe
Aurooba Ahmed <aurooba@auroobamakes.com>
2023-05-01 18:39:44 -0600

    Update scheduled-deploy.js

--- functions/scheduled-deploy.js
+++ functions/scheduled-deploy.js
@@ -2,7 +2,7 @@ import fetch from "node-fetch";
 import { schedule } from "@netlify/functions";
 
 // This is a sample build hook URL
-const BUILD_HOOK = "https://api.netlify.com/build_hooks/abc123def456";
+const BUILD_HOOK = "https://api.netlify.com/build_hooks/63df361be4658526ab374a75";
 
 // Schedules the handler function to run at midnight on
 // Mondays, Wednesday, and Friday

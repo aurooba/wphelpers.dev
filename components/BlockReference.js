a46382b85e68ea3c870b41677a5bc85aad1abf8b
Aurooba Ahmed <hey@aurooba.com>
2023-05-04 11:28:33 -0600

    add a close button and move details to an expandable area underneath on select

--- components/BlockReference.js
+++ components/BlockReference.js
@@ -45,54 +45,79 @@ export default function BlockReference(blockObject) {
 	};
 
 	return (
-		<div
-			className={`block-reference-${block.name} ${
-				displayProperties ? "block-reference--open" : "block-reference--closed"
-			}`}
-			key={"block-reference-" + block.name}>
-			<header>
-				<h3 className="block-reference__title">
-					{icon && <icons.Icon icon={icon} />}{" "}
-					<span className="visually-hidden">Title: </span> {title}
-				</h3>
+		<>
+			<div
+				className={`block-reference-${block.name}`}
+				key={"block-reference-" + block.name}>
+				<header>
+					<h3 className="block-reference__title">
+						{icon && <icons.Icon icon={icon} />}{" "}
+						<span className="visually-hidden">Title: </span> {title}
+					</h3>
 
-				<div className="block-reference__category">
-					<span className="visually-hidden">Category: </span>
-					<span>{category}</span>
+					<div className="block-reference__category">
+						<span className="visually-hidden">Category: </span>
+						<span>{category}</span>
+					</div>
+					<p className="block-reference__description">{description}</p>
+				</header>
+
+				<div className="button-wrapper">
+					<button
+						className="block-reference__toggle-properties"
+						onClick={() => setDisplayProperties(!displayProperties)}>
+						{displayProperties ? "Hide Properties" : "Show Properties"}
+					</button>
 				</div>
-				<p className="block-reference__description">{description}</p>
-			</header>
-
-			<div className="button-wrapper">
-				<button
-					className="block-reference__toggle-properties"
-					onClick={() => setDisplayProperties(!displayProperties)}>
-					{displayProperties ? "Hide Properties" : "Show Properties"}
-				</button>
 			</div>
 			{displayProperties && (
-				<div className="block-reference__content">
-					<div className="block-reference__properties">
-						{
-							// map over the properties object and display the key and value
-							Object.keys(properties).map((property) => {
-								return (
-									<div
-										className="block-reference__property"
-										key={property + "general-property-wrap"}>
-										<span className="block-reference__property-key">
-											{property}:
-										</span>
-										<div className="block-reference__property-value">
-											{stringify(properties[property])}
+				<div
+					className={`block-reference-${block.name} ${
+						displayProperties
+							? "block-reference--open"
+							: "block-reference--closed"
+					}`}
+					key={"block-reference-open-" + block.name}>
+					<header>
+						<div className="block-reference__close">
+							<button onClick={() => setDisplayProperties(false)}>
+								<icons.Icon icon={icons.close} />
+							</button>
+						</div>
+
+						<h3 className="block-reference__title">
+							{icon && <icons.Icon icon={icon} />}{" "}
+							<span className="visually-hidden">Title: </span> {title}
+						</h3>
+						<div className="block-reference__category">
+							<span className="visually-hidden">Category: </span>
+							<span>{category}</span>
+						</div>
+						<p className="block-reference__description">{description}</p>
+					</header>
+					<div className="block-content">
+						<div className="block-reference__properties">
+							{
+								// map over the properties object and display the key and value
+								Object.keys(properties).map((property) => {
+									return (
+										<div
+											className="block-reference__property"
+											key={property + "general-property-wrap"}>
+											<span className="block-reference__property-key">
+												{property}:
+											</span>
+											<div className="block-reference__property-value">
+												{stringify(properties[property])}
+											</div>
 										</div>
-									</div>
-								);
-							})
-						}
+									);
+								})
+							}
+						</div>
 					</div>
 				</div>
 			)}
-		</div>
+		</>
 	);
 }

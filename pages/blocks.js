d85537366f78a31bd299e0aa0d1e01830af1dd85
Aurooba Ahmed <hey@aurooba.com>
2023-05-03 19:41:00 -0600

    basic blocks page

---
+++ pages/blocks.js
@@ -0,0 +1,85 @@
+/**
+ * External Dependencies
+ */
+import Head from "next/head";
+import React, { useState, useEffect } from "react";
+
+/**
+ * Internal Dependencies
+ */
+import Footer from "@components/Footer";
+import BlockReference from "@components/BlockReference";
+
+export default function Icons() {
+	const [blocksObject, setBlocksObject] = useState({});
+	useEffect(() => {
+		fetch("/api/core-blocks")
+			.then(async (response) => response.json())
+			.then((response) => {
+				setBlocksObject(response);
+			})
+			.catch((error) => {
+				console.log("error: " + error);
+			});
+	}, []);
+	return (
+		<div className="container">
+			<Head>
+				<title>WordPress Icons Library.</title>
+				<link rel="icon" href="/favicon.ico" />
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<meta charSet="utf-8" />
+				<meta
+					name="description"
+					content="WordPress Core Blocks and their information."></meta>
+				<meta
+					property="og:title"
+					content={`WordPress Core Blocks Reference.`}
+					key="ogtitle"
+				/>
+				<meta
+					property="og:description"
+					content="WordPress Core Blocks and their information."
+					key="ogdesc"
+				/>
+				{/* Twitter */}
+				<meta name="twitter:card" content="summary_large_image" key="twcard" />
+				<meta name="twitter:creator" content="@aurooba" key="twhandle" />
+
+				{/* Open Graph */}
+				<meta property="og:url" content="https://latestwp.is" key="ogurl" />
+				<meta
+					property="og:image"
+					content="https://latestwp.is/latestwpis.png"
+					key="ogimage"
+				/>
+				<meta
+					property="og:site_name"
+					content={`WordPress Core Blocks Reference.`}
+					key="ogsitename"
+				/>
+				<meta
+					property="og:title"
+					content={`WordPress Core Blocks Reference.`}
+					key="ogtitle"
+				/>
+				<meta
+					property="og:description"
+					content="WordPress Core Blocks and their information."
+					key="ogdesc"
+				/>
+			</Head>
+
+			<main>
+				{
+					// map over the blocksObject and pass the block to the BlockReference component
+					Object.keys(blocksObject).map((block) => {
+						return <BlockReference block={blocksObject[block]["block"]} />;
+					})
+				}
+			</main>
+
+			<Footer />
+		</div>
+	);
+}

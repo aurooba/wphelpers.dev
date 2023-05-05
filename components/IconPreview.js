e6dd14808c6b2048f3ef52895291067eb2bd85ea
Aurooba Ahmed <hey@aurooba.com>
2023-05-05 11:46:49 -0600

    fix ref error in iconPreview

--- components/IconPreview.js
+++ components/IconPreview.js
@@ -1,16 +1,21 @@
 
 import  * as icons from '@wordpress/icons';
 import Link from 'next/link';
+import { forwardRef } from "react";
 
 
 export default function IconPreview({icon, name}) {
-
+	const IconComponent = forwardRef(function IconComponent(props, ref) {
+		return <icons.Icon icon={name} size={64} />;
+	});
 	return (
-		<div className="icon-card">	
-			<Link href={`/icons/${encodeURIComponent(icon)}`}><icons.Icon icon={ name } size={64} /></Link>
+		<div className="icon-card">
+			<Link href={`/icons/${encodeURIComponent(icon)}`}>
+				<IconComponent />
+			</Link>
 			<span>
 				<Link href={`/icons/${encodeURIComponent(icon)}`}>{icon}</Link>
 			</span>
 		</div>
-	)
+	);
 }

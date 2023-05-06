00d3c269a56946ea837d6686779f598f7cde7727
Aurooba Ahmed <hey@aurooba.com>
2023-05-06 00:42:38 -0600

    add copy click event

--- components/IconCard.js
+++ components/IconCard.js
@@ -1,52 +1,65 @@
-import { useState } from 'react';
-import  * as icons from '@wordpress/icons';
-import { renderToStaticMarkup } from 'react-dom/server';
+import { useState } from "react";
+import * as icons from "@wordpress/icons";
+import { renderToStaticMarkup } from "react-dom/server";
+import * as Fathom from "fathom-client";
 
-export default function IconCard({icon}) {
-
+export default function IconCard({ icon }) {
 	const [isCopied, setIsCopied] = useState(false);
 
 	const importText = `import { Icon, ${icon} } from '@wordpress/icons';`;
 	const iconText = `<Icon icon={${icon}} />`;
 
 	async function handleCopyClick(text) {
+		Fathom.trackGoal("QQWKGXO3", 0);
 		setIsCopied(true);
-        setTimeout(() => {
-          setIsCopied(false);
-        }, 1500);
+		setTimeout(() => {
+			setIsCopied(false);
+		}, 1500);
 		return await navigator.clipboard.writeText(text);
 	}
 
-
 	return (
-		<div className="icon-card">
-			<icons.Icon icon={ icons[icon] } size={64} />
+		<div id="icon-card" className="icon-card">
+			<icons.Icon icon={icons[icon]} size={64} />
 
-			<button onClick={() => { handleCopyClick(icon)}} className="no-border">
+			<button
+				onClick={() => {
+					handleCopyClick(icon);
+				}}
+				className="no-border copy-button">
 				<span className="visually-hidden">Click to copy</span>
 				<h2>{icon}</h2>
-				<icons.Icon icon={ icons["copy"] } size={24}/> 
+				<icons.Icon icon={icons["copy"]} size={24} />
 			</button>
-			
-			
-			<button onClick={() => { handleCopyClick(importText)}}>
+
+			<button
+				onClick={() => {
+					handleCopyClick(importText);
+				}}
+				className="copy-button">
 				<span className="visually-hidden">Click to copy</span>
 				<pre>{importText}</pre>
-				<icons.Icon icon={ icons["copy"] } size={24}/>
+				<icons.Icon icon={icons["copy"]} size={24} />
 			</button>
-			<button onClick={() => { handleCopyClick(iconText)}}>
+			<button
+				onClick={() => {
+					handleCopyClick(iconText);
+				}}
+				className="copy-button">
 				<span className="visually-hidden">Click to copy</span>
 				<pre>{iconText}</pre>
-				<icons.Icon icon={ icons["copy"] } size={24}/>
+				<icons.Icon icon={icons["copy"]} size={24} />
 			</button>
-			<button onClick={() => { handleCopyClick(renderToStaticMarkup(icons[icon]))}}>
+			<button
+				onClick={() => {
+					handleCopyClick(renderToStaticMarkup(icons[icon]));
+				}}>
 				<span className="visually-hidden">Click to copy</span>
 				<pre>{renderToStaticMarkup(icons[icon])}</pre>
-				<icons.Icon icon={ icons["copy"] } size={24}/>
+				<icons.Icon icon={icons["copy"]} size={24} />
 			</button>
-			
 
 			{isCopied && <span className="copied">Copied!</span>}
 		</div>
-	)
+	);
 }

d95d74c86badaa2650a88876cb8aed83c6b3b9ca
Aurooba Ahmed <hey@aurooba.com>
2023-08-05 23:40:05 -0600

    add github link to footer

--- components/Footer.js
+++ components/Footer.js
@@ -6,10 +6,18 @@ export default function Footer() {
 					Built by{" "}
 					<a href="https://aurooba.com" target="_blank" rel="noopener">
 						Aurooba
-					</a>
-					{" "}and{" "}
+					</a>{" "}
+					and{" "}
 					<a href="https://www.briancoords.com" target="_blank" rel="noopener">
 						Brian
+					</a>
+				</p>
+				<p>
+					<a
+						href="https://github.com/aurooba/wphelpers.dev"
+						target="_blank"
+						rel="noopener">
+						View on GitHub
 					</a>
 				</p>
 			</footer>
